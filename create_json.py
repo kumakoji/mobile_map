6b6feaa25437d9830b92e9fb25f25bf4feb819d7
KumanamiKoji <kuma.koji922@gmail.com>
2014-09-06 00:03:48 +0900

    add make_json.py

---
+++ create_json.py
@@ -0,0 +1,37 @@
+#!/bin/py
+import sys
+argvs = sys.argv
+
+def main():
+	f = init()
+	log = input_log()
+	output_geo(log,f)
+
+def init():
+	f = open('map.json','w');
+	f.write('var geodata = {"Marker":[\n{')
+	return f
+
+def input_log():
+	log = []
+	content = ""
+	for line in open(argvs[1],'r'):
+		if 'AlarmManager' in line:
+			content = '"content":"'+content+'"'
+			log.append(lat+','+lng+','+content)	
+			content = ""
+		elif 'Location ' in line:
+			lat = line[:-1].split('\t')[1].split(': ')[1].split(', ')[0].replace("(","")
+			lng = line[:-1].split('\t')[1].split(': ')[1].split(', ')[1].replace(")","")
+			lat = '"lat":"'+lat+'"'
+			lng = '"lng":"'+lng+'"'
+		content += line[:-1]+"<br>"
+	return log
+
+def output_geo(log,f):
+	f.write(('\n},\n{\n').join(log))
+	f.write('}\n]}\n')
+	f.close()
+
+
+main()	
